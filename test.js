51b0789c6e6baa4c505027a8246a92eb66c99c95
ankitsarwal7 <ankitsarwal7@gmail.com>
2025-03-23 12:34:31 +0530

    initial

--- test.js
+++ test.js
@@ -33,14 +33,59 @@ class DoublyLinkedList{
     this.tail.next = newNode
     newNode.prev = this.tail
     this.tail = newNode
-
+ 
     this.length++
     return this;
 
   }
  
+ //pop method
+
+  pop(){
+    
+    if(this.length===0){
+      return undefined
+    }
+    let temp = this.tail
+
+    if (this.length===1) {
+      this.head = null
+      this.tail = null
+    }
+    this.tail = this.tail.prev
+    this.tail.next = null
+    temp.prev = null
+    this.length--
+    return temp;
+  }
+ 
+  // unshift method its add element in starting the list
+
+  unshift(value){
+    let newNode = new Node(value)
+    
+    if (this.length===0){
+      this.head = newNode
+      this.tail = newNode
+     }
+      
+
+
+
+      
+    }
+
+
+  }
+
+
+
 }
 const myDoublyLinkedList = new DoublyLinkedList(0)
 
 myDoublyLinkedList.push(1)
-console.log(myDoublyLinkedList);
+myDoublyLinkedList.push(2)
+myDoublyLinkedList.push(3)
+ 
+console.log(myDoublyLinkedList.pop())
+ 
